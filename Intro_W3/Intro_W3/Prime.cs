5bcbfa978fd37fbdd40ae82a6ff7eb68a2605f3d
Thomas van den Heuvel <thomas.van.den.heuvel@ciber.nl>
2021-09-14 16:18:58 +0200

    Azure_Bootcamp

---
+++ Intro_W3/Intro_W3/Prime.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro_W3
+{
+    class Prime
+    {
+        //Variables
+        // accessmodifier type name (= value);
+        private bool nonprime;
+        public List<int> primes;
+
+        // constructor
+        public Prime()
+        {
+            primes = new List<int>();
+        }
+
+        // methods
+        // accessmodifier returntype name(){}
+
+        public void GetPrimeNumbers(int maxrange)
+        {
+            for(int i = 2; i < (maxrange+1); i++)
+            {
+                for(int y= 2; y< i; y++)
+                {
+                    if(i % y == 0)
+                    {
+                        nonprime = true;
+                        break; 
+                    }
+                }
+                if (!nonprime)
+                {
+                    primes.Add(i);
+                }
+                nonprime = false;
+            }
+        }
+    }
+}
---
+++ Intro_W3/Intro_W3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Intro_W3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prime prime1 = new Prime();
+            prime1.GetPrimeNumbers(150);
+            for(int i =0; i < prime1.primes.Count; i++)
+            {
+                Console.WriteLine(prime1.primes[i]);            
+            }
+        }
+    }
+}
+
+
